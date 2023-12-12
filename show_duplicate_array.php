e8f9258d09da6d1a181def6c79ef86258e071a0e
prasunmatrix <prasun@matrixnmedia.com>
2023-12-12 22:32:03 +0530

    once again show duplicate value in a array

--- show_duplicate_array.php
+++ show_duplicate_array.php
@@ -14,3 +14,28 @@ foreach($array as $val)
 }
 echo "<pre>";
 print_r($newarray);
+
+$array = array(1, 6, 2, 23, 10, 15, 3, 2, 15, 7);
+$newarr=array();
+foreach($array as $key=>$val)
+{
+  if(isset($newarr[$val]))
+  {
+    $newarr[$val]=$val;
+  }
+  else
+  {
+    $newarr[$val]=0;
+  }
+}
+echo "<pre>";
+print_r($newarr);
+
+foreach($newarr as $key2=>$val2)
+{
+  if($val2>0)
+  {
+    echo $val2;
+    echo "\n";
+  }
+}
