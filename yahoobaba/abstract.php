b2018a3f1625db609720efd68b6181ba1fa9e866
prasunmatrix <prasun@matrixnmedia.com>
2024-04-16 16:41:16 +0530

    changes in abstract.php file

--- yahoobaba/abstract.php
+++ yahoobaba/abstract.php
@@ -3,6 +3,12 @@ abstract class parentClass
 {
   public $name;
 
+  public function __construct($name)
+  {
+    echo $this->name=$name;
+    echo "<br>";
+  }
+
   abstract protected function calc($a, $b);
 }
 
@@ -22,6 +28,7 @@ class childClass extends parentClass
 
 //$obj=new parentClass();
 
-$obj1=new childClass();
+$obj1=new childClass("Rimu");
+$obj1->name;
 
 $obj1->calc(10,20);
